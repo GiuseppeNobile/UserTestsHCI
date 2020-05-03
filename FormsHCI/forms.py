c21ae821a4cb7ae2a2796de50592aea14ecb9a67
GiuseppeNobile <giffo.640@gmail.com>
2020-05-03 16:44:34 +0200

    aggiunto bootstrap e corretto widget risposte

--- FormsHCI/forms.py
+++ FormsHCI/forms.py
@@ -15,7 +15,6 @@ class TestForm(forms.Form):
 
 
 class SUSForm(forms.ModelForm):
-
     class Meta:
         model = Domanda
         fields = ('testoDomanda',)
@@ -28,35 +27,35 @@ class SUSForm(forms.ModelForm):
 
     FIELDS = {
         Domanda.testoDomanda: forms.CharField,
-        RispostaSUS.scelta: forms.IntegerField
+        RispostaSUS.scelta: forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
     }
 
     domanda01 = "I think that I would like to use this system frequently."
-    risposta01 = forms.CheckboxSelectMultiple(choices=OPTIONS)
+    risposta01 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda02 = "I found the system unnecessarily complex."
-    risposta02 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta02 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda03 = "I thought the system was easy to use."
-    risposta03 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta03 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda04 = "I think that I would need the support of a technical person to be able to use this system."
-    risposta04 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta04 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda05 = "I found the various functions in this system were well integrated."
-    risposta05 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta05 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda06 = "I thought there was too much inconsistency in this system."
-    risposta06 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta06 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda07 = "I would imagine that most people would learn to use this system very quickly."
-    risposta07 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta07 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda08 = "I found the system very cumbersome to use."
-    risposta08 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta08 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda09 = "I felt very confident using the system."
-    risposta09 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta09 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
 
     domanda10 = "I needed to learn a lot of things before I could get going with this system."
-    risposta10 = forms.MultipleChoiceField(choices=OPTIONS)
+    risposta10 = forms.ChoiceField(choices=OPTIONS, widget=forms.RadioSelect)
